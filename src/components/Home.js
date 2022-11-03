9a46cd558782f53d9367118540d23c4dba266111
Pooja Singh <poojasingh1234gkp@gmail.com>
2022-11-03 11:58:21 +0000

    created dropdown

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Dropdown from './dropdown';
+import './home.css';
 
 const Home = () => {
   return (
-    <div>Home component</div>
+    <div>Home component
+      <div className='container'>
+        <div className='dropdown1'>
+          <Dropdown />
+        </div>
+        <div className='dropdown2'>
+          <Dropdown />
+        </div>
+        <div className='dropdown3'>
+          <Dropdown />
+        </div>
+        <div className='dropdown4'>
+          <Dropdown />
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import './dropdown.css';
 const Dropdown = () => {
+  const options = ["Select", "DonLon", "Enchai", "Jebing", "Sapir", "Lerbin", "Pingasor"];
+  const [value, setValue] = useState("Select");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
-    <div>dropdown</div>
-  )
+    <div>
+      <form>
+        <select
+          value={value}
+          onChange={handleChange}>
+          {options.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </form>
+    </div>
+  );
 }
-
 export default Dropdown;
