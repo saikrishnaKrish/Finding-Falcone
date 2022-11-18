bf71c2cda7cca87eab44d6158c9b8f6bc6e975db
Sai Krishna Kanteti <82278675+saikrishnaKrish@users.noreply.github.com>
2022-11-18 18:04:11 +0000

    fixed radio button inputs

--- src/components/radioButtonComp.js
+++ src/components/radioButtonComp.js
@@ -5,20 +5,25 @@ const RadioButtonComp = ({vehiclesData, rname, updateVehicles}) => {
   // console.log(vehiclesData)
   const [selectVehicle,setSelectVehicle]=useState('')
   const handleChange=(event)=>{
-    event.preventDefault();
+   
     let selectedVehicle=event.target.value;
+    console.log(selectedVehicle)
     let filteredSelectedOption=vehiclesData.filter((vd)=>vd.name!=selectedVehicle)
     console.log(filteredSelectedOption)
     setSelectVehicle(selectedVehicle)
     updateVehicles(filteredSelectedOption,selectedVehicle)
   }
   return (
-    <div onChange={(e) => handleChange(e)}>
+    <div >
       {vehiclesData.map((vd) => {
         return (
           <div key={vd.key}>
             <label for={vd.key}>{vd.name} </label>
-            <input type="radio"  name={rname} value={vd.name} />
+            <input type="radio"  name={rname}
+             value={vd.name} 
+            checked={selectVehicle == vd.name}
+            onChange={handleChange}
+            />
           </div>
         );
       })}
